2336a3dcc44ceddec909024e77d28bebe2dea7f6
Lordominus <daniel.idiaconu@gmail.com>
2024-01-30 22:58:54 +0000

    JS updated with furteher functions, icons from openweather, listeners, etc

--- Assets/js/WeatherApp.js
+++ Assets/js/WeatherApp.js
@@ -92,3 +92,119 @@ $(function() {
 
         // Build 5 day forecast URL for the current day and retrieve longitude and latitude
         const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}`
+
+        
+        // Fetch 5 day forecast
+        fetch(forecastURL)
+          .then(function(response) {
+            return response.json()
+          })
+          .then(function(data) {
+            // List of all times
+            const list = data.list
+            
+            // Get relevant data from Forecast Object and store in forecastSnapshots array
+            const forecastSnapshots = []
+            for (let i = 1; i < 6; i++) {
+              let snapshot;
+              // Try to get noon data
+              if (snapshot = data.list.find((time) => time.dt == getUnixCode(i, 12))) {
+                forecastSnapshots.push(snapshot)
+                continue
+              }
+              // If noon data isnt available, get earliest possible time
+              if (snapshot = data.list.find((time) => time.dt == getUnixCode(i, 0))) {
+                forecastSnapshots.push(snapshot)
+                continue
+              }
+              throw new Error(`Could not retrieve raw date element for ${i}th element of snapshot list`);
+            }
+
+            // Record 5 day forecast to DOM
+            for (let i = 1; i < 6; i++) {
+              // Dynamically get DOM elements
+              let dateEl = $('#day'+i+'-date')
+              let tempEl = $('#day'+i+'-temp')
+              let windEl = $('#day'+i+'-wind')
+              let humidityEl = $('#day'+i+'-humidity')
+              let iconEl = $('#day'+i+'-icon')
+
+              // Retrieve data from forecastSnapshots
+              temp = (forecastSnapshots[i - 1].main.temp - 273.15).toFixed(2) +'°'
+              wind = forecastSnapshots[i - 1].wind.speed +'KPH'
+              humidity = forecastSnapshots[i - 1].main.humidity + '%'
+              icon = forecastSnapshots[i - 1].weather[0].icon
+
+              // Record to DOM
+              dateEl.text(today.add(i, 'd').format('DD/MM/YY'))
+              tempEl.text(temp)
+              windEl.text(wind)
+              humidityEl.text(humidity)
+              iconEl.attr('src', iconURL(icon))
+            }
+          })
+        })
+      })
+  })
+})
+
+// Function to build icon source URL
+function iconURL(iconCode) {
+  return `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+}
+
+// Return unix code for given day / hour
+function getUnixCode(day, hour) {
+  let unix = dayjs()
+    .add(day, 'd')
+    .hour(hour)
+    .startOf('hour')
+    .unix()
+  return unix;
+}
+
+// Create list elements and append to history search results
+function displaySearchHistory(resultsEl, input, submit) {
+  resultsEl.empty()
+  let results = history.map(item => `
+    <li data-result="${item}">
+      <i class="fa-solid fa-location-dot"></i>
+      ${item}
+    </li>
+  `)
+  resultsEl.html(results.join(""))
+
+  // Add event listener to list items
+  $('#search-history-container li').on('click',{input, submit}, loadHistoryResult)
+}
+
+// Stringify history array and store in local storage
+function storeSearchHistory(location) {
+  const index = history.indexOf(location);
+  if (index != -1) history.splice(index, 1)
+  history.unshift(location)
+  localStorage.setItem('history', JSON.stringify(history))
+}
+
+// Set up event listeners to display and hide search results
+function displaySearch(input) {
+  // Display search results
+  const results = $('#search-history-results')
+  input.focus(() => {
+    results.removeClass('hidden')
+  })
+  
+  // Hide search results - add timeout to allow results event listener priority
+  input.blur(() => {
+    setTimeout(() => results.addClass('hidden'), 100)
+  })
+}
+
+// Load history result into search bar and initiate search
+function loadHistoryResult(event) {
+  let result = $(this).data('result')
+  event.data.input.val(result)
+  event.data.submit.trigger('click')
+}
+
+//  The End, Thank you for reading my magical code, you are great!!!
